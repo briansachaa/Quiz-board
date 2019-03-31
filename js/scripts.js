ca7512dea8b70b633e2240b16d11811b9ac601d4
briansachaa <briansachaasang@gmail.com>
2019-03-31 22:03:34 +0300

    add final js code

--- js/scripts.js
+++ js/scripts.js
@@ -15,5 +15,53 @@ $(document).ready(function() {
           alert("please answer all questions")
       }
 
+      function results() {
+        return nameInput + "you got" + correct;
+      }
+
+      var correct = 0
+      if(question1 == answers[0]) {
+        correct += 20;
+      }else if (question1 !=answers[0]) {
+        correct += 0;
+      }
+      if(question2 == answers[1]) {
+        correct += 20;
+      }else if (question2 !=answers[1]) {
+        correct += 0;
+      }
+      if(question3 == answers[2]) {
+        correct += 20;
+      }else if (question2 !=answers[2]) {
+        correct += 0;
+      }
+      if(question4 == answers[3]) {
+        correct += 20;
+      }else if (question4 !=answers[3]) {
+        correct += 0;
+      }
+      if(question5 == answers[4]) {
+        correct += 20;
+      }else if (question5 !=answers[4]) {
+        correct += 0;
+      }
+    var messages = ["congrats", "passed", "It is important you repeat this test"]
+		var photo = ["img/congrats.jpg", "img/pass.jpg", "images/fail.jpg", ]
+		var score;
+		if (correct >= 80) {
+			score = 0;
+		} else if (correct >= 50 && correct < 80) {
+			score = 1;
+		} else if (correct <= 30 && correct < 50) {
+			score = 2;
+		}
+		document.getElementById("messages").innerHTML = messages[score];
+		document.getElementById("results").innerHTML = results();
+		document.getElementById("photo").src = photo[score];
+
+		function myFunction() {
+			document.getElementById("retake").style.visibilty = "visible";
+		}
+		event.preventDefault();
   })
 })
